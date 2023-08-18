6785175ba511ac747fd6d11653a747a50f918c23
Danilo Bellini <bellini.daan@gmail.com>
2023-08-18 15:45:40 -0300

    Implementado o BrowserRouter e início do componente Login

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <form action="">
+      <input
+        type="text"
+        placeholder="Email"
+        data-testid="email-input"
+      />
+    </form>
+  );
+}
+
+export default Login;
