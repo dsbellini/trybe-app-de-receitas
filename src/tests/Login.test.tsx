7df68020078a14f2efeeb7d436a0a6cc8321d3e4
Danilo Bellini <bellini.daan@gmail.com>
2023-08-24 18:38:48 -0300

    Tentando corrigir test Login

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,10 +1,26 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+// import { mockMeal, mockDrink } from './mocks/mockMealsAndDrinks';
 
 describe('Testa a página de Login', () => {
+  beforeEach(() => {
+    const mockFetch = () => Promise.resolve({
+      json: () => Promise.resolve({}),
+    });
+
+    // vi.spyOn(global, 'fetch').mockImplementation(mockFetch as any);
+    global.fetch = mockFetch as any;
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   test('Verifica se a página contém inputs de email, senha e botão de entrar', () => {
+    // expect(global.fetch).toHaveBeenCalledTimes(0);
     renderWithRouter(<App />, { route: '/' });
     const email = screen.getByTestId(/email-input/i);
     const senha = screen.getByTestId(/password-input/i);
@@ -26,3 +42,47 @@ describe('Testa a página de Login', () => {
     expect(botao).toBeEnabled();
   });
 });
+
+// beforeEach(() => {
+//   // Mock para a função fetch
+//   const fetch = (url: string) => Promise.resolve({
+//     status: 200,
+//     ok: true,
+//     json: () => {
+//       if (url === MEALS_URL) {
+//         return Promise.resolve({
+//           status: 200,
+//           ok: true,
+//           json: () => Promise.resolve(mealsData),
+//         });
+//       }
+//       if (url === DRINKS_URL) {
+//         return Promise.resolve({
+//           status: 200,
+//           ok: true,
+//           json: () => Promise.resolve(drinksData),
+
+//         });
+//       }
+
+//       if (url === 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list') {
+//         return Promise.resolve({
+//           status: 200,
+//           ok: true,
+//           json: () => Promise.resolve({
+//             drinks: [{ strCategory: 'Ordinary Drink' }, {
+//               strCategory: 'Cocktail',
+//             }],
+//           }),
+//         });
+//       }
+//     },
+//   });
+
+//   // Mock do fetch global
+//   global.fetch = fetch as any;
+// });
+
+// afterEach(() => {
+//   vi.clearAllMocks();
+// });
---
+++ src/tests/mocks/mockSearchMealAndDrinks.ts
@@ -0,0 +1,249 @@
+export const mockSearchmeal = {
+  meals: [
+    {
+      strMeal: 'Baked salmon with fennel & tomatoes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1548772327.jpg',
+      idMeal: '52959',
+    },
+    {
+      strMeal: 'Cajun spiced fish tacos',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg',
+      idMeal: '52819',
+    },
+    {
+      strMeal: 'Escovitch Fish',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1520084413.jpg',
+      idMeal: '52944',
+    },
+    {
+      strMeal: 'Fish fofos',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg',
+      idMeal: '53043',
+    },
+    {
+      strMeal: 'Fish pie',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg',
+      idMeal: '52802',
+    },
+    {
+      strMeal: 'Fish Stew with Rouille',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vptqpw1511798500.jpg',
+      idMeal: '52918',
+    },
+    {
+      strMeal: 'Garides Saganaki',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wuvryu1468232995.jpg',
+      idMeal: '52764',
+    },
+    {
+      strMeal: 'Grilled Portuguese sardines',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/lpd4wy1614347943.jpg',
+      idMeal: '53041',
+    },
+    {
+      strMeal: 'Honey Teriyaki Salmon',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xxyupu1468262513.jpg',
+      idMeal: '52773',
+    },
+    {
+      strMeal: 'Kedgeree',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/utxqpt1511639216.jpg',
+      idMeal: '52887',
+    },
+    {
+      strMeal: 'Kung Po Prawns',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1525873040.jpg',
+      idMeal: '52946',
+    },
+    {
+      strMeal: 'Laksa King Prawn Noodles',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/rvypwy1503069308.jpg',
+      idMeal: '52821',
+    },
+    {
+      strMeal: 'Mediterranean Pasta Salad',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wvqpwt1468339226.jpg',
+      idMeal: '52777',
+    },
+    {
+      strMeal: 'Mee goreng mamak',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xquakq1619787532.jpg',
+      idMeal: '53048',
+    },
+    {
+      strMeal: 'Nasi lemak',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wai9bw1619788844.jpg',
+      idMeal: '53051',
+    },
+    {
+      strMeal: 'Portuguese fish stew (Caldeirada de peixe)',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/do7zps1614349775.jpg',
+      idMeal: '53045',
+    },
+    {
+      strMeal: 'Recheado Masala Fish',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/uwxusv1487344500.jpg',
+      idMeal: '52809',
+    },
+    {
+      strMeal: 'Salmon Avocado Salad',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1549542994.jpg',
+      idMeal: '52960',
+    },
+    {
+      strMeal: 'Salmon Prawn Risotto',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xxrxux1503070723.jpg',
+      idMeal: '52823',
+    },
+    {
+      strMeal: 'Saltfish and Ackee',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vytypy1511883765.jpg',
+      idMeal: '52936',
+    },
+    {
+      strMeal: 'Seafood fideuà',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wqqvyq1511179730.jpg',
+      idMeal: '52836',
+    },
+    {
+      strMeal: 'Shrimp Chow Fun',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1529445434.jpg',
+      idMeal: '52953',
+    },
+    {
+      strMeal: 'Sledz w Oleju (Polish Herrings)',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/7ttta31593350374.jpg',
+      idMeal: '53023',
+    },
+    {
+      strMeal: 'Spring onion and prawn empanadas',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1c5oso1614347493.jpg',
+      idMeal: '53040',
+    },
+    {
+      strMeal: 'Sushi',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg',
+      idMeal: '53065',
+    },
+    {
+      strMeal: 'Three Fish Pie',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/spswqs1511558697.jpg',
+      idMeal: '52882',
+    },
+    {
+      strMeal: 'Tuna and Egg Briks',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/2dsltq1560461468.jpg',
+      idMeal: '52975',
+    },
+    {
+      strMeal: 'Tuna Nicoise',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/yypwwq1511304979.jpg',
+      idMeal: '52852',
+    },
+  ],
+};
+
+export const mockSearchDrink = {
+  drinks: [
+    {
+      strDrink: '3-Mile Long Island Iced Tea',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg',
+      idDrink: '15300',
+    },
+    {
+      strDrink: '410 Gone',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xtuyqv1472669026.jpg',
+      idDrink: '13581',
+    },
+    {
+      strDrink: '50/50',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/wwpyvr1461919316.jpg',
+      idDrink: '14598',
+    },
+    {
+      strDrink: '501 Blue',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/ywxwqs1461867097.jpg',
+      idDrink: '17105',
+    },
+    {
+      strDrink: '69 Special',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg',
+      idDrink: '13940',
+    },
+    {
+      strDrink: 'A Day at the Beach',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/trptts1454514474.jpg',
+      idDrink: '15200',
+    },
+    {
+      strDrink: 'A Furlong Too Late',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/ssxvww1472669166.jpg',
+      idDrink: '17831',
+    },
+    {
+      strDrink: 'A midsummernight dream',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/ysqvqp1461867292.jpg',
+      idDrink: '15675',
+    },
+    {
+      strDrink: 'A Night In Old Mandalay',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyrvxt1461919380.jpg',
+      idDrink: '17832',
+    },
+    {
+      strDrink: 'A. J.',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/l74qo91582480316.jpg',
+      idDrink: '17833',
+    },
+    {
+      strDrink: 'Abbey Cocktail',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/mr30ob1582479875.jpg',
+      idDrink: '17834',
+    },
+    {
+      strDrink: 'Abilene',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/smb2oe1582479072.jpg',
+      idDrink: '17835',
+    },
+    {
+      strDrink: 'Absolut Stress #2',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xuyqrw1472811825.jpg',
+      idDrink: '15597',
+    },
+    {
+      strDrink: 'Absolutely Cranberry Smash',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vqwstv1472811884.jpg',
+      idDrink: '15024',
+    },
+    {
+      strDrink: 'Acapulco',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/il9e0r1582478841.jpg',
+      idDrink: '17836',
+    },
+    {
+      strDrink: 'Adam',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/v0at4i1582478473.jpg',
+      idDrink: '17837',
+    },
+    {
+      strDrink: 'Adam Sunrise',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vtuyvu1472812112.jpg',
+      idDrink: '15567',
+    },
+    {
+      strDrink: 'Adonis Cocktail',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xrvruq1472812030.jpg',
+      idDrink: '17838',
+    },
+    {
+      strDrink: 'Affair',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/h5za6y1582477994.jpg',
+      idDrink: '17839',
+    },
+    {
+      strDrink: 'Affinity',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/wzdtnn1582477684.jpg',
+      idDrink: '17840',
+    },
+  ],
+};
